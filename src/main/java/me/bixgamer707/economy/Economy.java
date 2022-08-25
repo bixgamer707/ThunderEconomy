16929ec3abe5972622917384304c22a6051689ed
bixDev <bixgamer707@gmail.com>
2022-08-24 21:19:33 -0500

    feat economy ] -> BankBuilder has been register bank "global" for the server ($$$) and register commands, listeners, placeholders...

--- src/main/java/me/bixgamer707/economy/Economy.java
+++ src/main/java/me/bixgamer707/economy/Economy.java
@@ -1,17 +1,27 @@
 package me.bixgamer707.economy;
 
 import me.bixgamer707.economy.api.bank.ProcessMethodEnum;
-import me.bixgamer707.economy.bank.helper.BankHelper;
+import me.bixgamer707.economy.bank.helper.BankBuilder;
 import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Economy extends JavaPlugin {
 
+    private static Economy instance;
+
     @Override
     public void onEnable() {
+        instance = this;
         // Plugin startup logic
-        BankHelper.build(
+        new BankBuilder();
+        registerCommands();
+        registerPlaceholders();
+        registerTabCompletions();
+        registerListeners();
+
+        BankBuilder.build(
                 ProcessMethodEnum.ASYNC,
                 "global*"
         );
@@ -20,6 +30,28 @@ public final class Economy extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        instance = null;
+    }
+
+    public void registerCommands(){
+        //getCommand("economy").setExecutor(new EconomyCommand(plugin));
+    }
+
+    public void registerTabCompletions(){
+
+    }
+
+    public void registerListeners(Listener... listeners){
+        for(Listener listener : listeners){
+            Bukkit.getPluginManager().registerEvents(listener, this);
+        }
+    }
+
+    public void registerPlaceholders(){
+
+    }
+
+    public static Economy getInstance() {
+        return instance;
     }
 }
